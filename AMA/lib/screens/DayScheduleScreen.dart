6ccd296ccdc8812723f6c299be520514b9b96b95
EduRibeiro00 <eribeiro306@gmail.com>
2019-10-22 19:15:28 +0100

    Deleted placeholder code for testing

--- AMA/lib/screens/DayScheduleScreen.dart
+++ AMA/lib/screens/DayScheduleScreen.dart
@@ -6,8 +6,6 @@ import 'package:ama/data/DayScheduleInfo.dart';
 import 'package:ama/data/DaySessionsInfo.dart';
 import 'package:ama/data/Session.dart';
 import 'package:flutter/material.dart';
-import '../components/SessionContainer.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import '../constants/AppColors.dart' as AppColors;
 import '../constants/Utility.dart' as Utility;
 
@@ -15,69 +13,6 @@ class DayScheduleScreen extends StatefulWidget {
   DayScheduleScreen({this.info});
 
   final DayScheduleInfo info;
-
-  static Session session1 = Session(
-      key: "dc03a3e9-5daf4-973c-0941b84ef00b",
-      title: "Student Research Competition: Oral Presentation",
-      chairStrings: ["Philipp Haller"],
-      chairKeys: ["philipphaller"],
-      itemKeys: [
-        "programming-2019-src-cantor-pairing-in-a-reversible-programming-language-short-presentation-",
-        "programming-2019-src-reading-logic-as-code-or-as-natural-language-text-short-presentation-",
-        "programming-2019-src-a-framework-for-big-step-semantics-short-presentation-",
-        "programming-2019-src-rml-runtime-monitoring-language-short-presentation-",
-        "programming-2019-src-baccaml-the-meta-hybrid-just-in-time-compiler-short-presentation-",
-        "programming-2019-src-magritte-a-modern-shell-language-short-presentation-"
-      ],
-      description:
-          "The ACM Student Research Competition (SRC), sponsored by Microsoft Research, offers a unique forum for ACM student members at the undergraduate and graduate levels to present their original research at &lt;Programming&gt; before a panel of judges and conference attendees, and creates opportunities for learning and networking among researchers and practitioners. The SRC gives visibility to not only up-and-coming young researchers, but also to the field of computer science research and its community.",
-      type: "Invited Students",
-      day: "2019-04-03",
-      timeString: "02:30 - 11:00",
-      startTime: DateTime.parse("2019-04-03 09:30"),
-      location: "Hotel Bristol Palace | Bellini");
-
-  static Session session2 = Session(
-      key: "dc03a3e9-5dbd-4af4-973c-0940b",
-      title: "Student Research Competition: Oral Presentation",
-      chairStrings: ["Philipp Haller"],
-      chairKeys: ["philipphaller"],
-      itemKeys: [
-        "programming-2019-src-cantor-pairing-in-a-reversible-programming-language-short-presentation-",
-        "programming-2019-src-reading-logic-as-code-or-as-natural-language-text-short-presentation-",
-        "programming-2019-src-a-framework-for-big-step-semantics-short-presentation-",
-        "programming-2019-src-rml-runtime-monitoring-language-short-presentation-",
-        "programming-2019-src-baccaml-the-meta-hybrid-just-in-time-compiler-short-presentation-",
-        "programming-2019-src-magritte-a-modern-shell-language-short-presentation-"
-      ],
-      description:
-          "The ACM Student Research Competition (SRC), sponsored by Microsoft Research, offers a unique forum for ACM student members at the undergraduate and graduate levels to present their original research at &lt;Programming&gt; before a panel of judges and conference attendees, and creates opportunities for learning and networking among researchers and practitioners. The SRC gives visibility to not only up-and-coming young researchers, but also to the field of computer science research and its community.",
-      type: "Invited Students",
-      day: "2019-04-03",
-      timeString: "08:30 - 11:00",
-      startTime: DateTime.parse("2019-04-03 09:30"),
-      location: "Hotel Bristol Palace | Bellini");
-
-  static Session session3 = Session(
-      key: "dc03a3e9-5dbd-4af4-973c-0941b84ef00b",
-      title: "Student Research Competition: Oral Presentation",
-      chairStrings: ["Philipp Haller"],
-      chairKeys: ["philipphaller"],
-      itemKeys: [
-        "programming-2019-src-cantor-pairing-in-a-reversible-programming-language-short-presentation-",
-        "programming-2019-src-reading-logic-as-code-or-as-natural-language-text-short-presentation-",
-        "programming-2019-src-a-framework-for-big-step-semantics-short-presentation-",
-        "programming-2019-src-rml-runtime-monitoring-language-short-presentation-",
-        "programming-2019-src-baccaml-the-meta-hybrid-just-in-time-compiler-short-presentation-",
-        "programming-2019-src-magritte-a-modern-shell-language-short-presentation-"
-      ],
-      description:
-          "The ACM Student Research Competition (SRC), sponsored by Microsoft Research, offers a unique forum for ACM student members at the undergraduate and graduate levels to present their original research at &lt;Programming&gt; before a panel of judges and conference attendees, and creates opportunities for learning and networking among researchers and practitioners. The SRC gives visibility to not only up-and-coming young researchers, but also to the field of computer science research and its community.",
-      type: "Invited Students",
-      day: "2019-04-03",
-      timeString: "10:30 - 11:00",
-      startTime: DateTime.parse("2019-04-03 09:30"),
-      location: "Hotel Bristol Palace | Bellini");
 
   @override
   DayScheduleScreenState createState() => DayScheduleScreenState();
@@ -150,149 +85,17 @@ class DayScheduleScreenState extends State<DayScheduleScreen> {
                               content: Text("Session deleted from schedule")));
                         },
                       );
-                    }
-
-                    /*children: <Widget>[
-                     SessionContainer(
-                    activity: Session(
-                        key: "dc03a3e9-5dbd-4af4-973c-0941b84ef00b",
-                        title: "Student Research Competition: Oral Presentation",
-                        chairString: "Philipp Haller",
-                        chairKey: "philipphaller",
-                        itemKeys: ["programming-2019-src-cantor-pairing-in-a-reversible-programming-language-short-presentation-",
-                                   "programming-2019-src-reading-logic-as-code-or-as-natural-language-text-short-presentation-",
-                                   "programming-2019-src-a-framework-for-big-step-semantics-short-presentation-",
-                                   "programming-2019-src-rml-runtime-monitoring-language-short-presentation-",
-                                   "programming-2019-src-baccaml-the-meta-hybrid-just-in-time-compiler-short-presentation-",
-                                   "programming-2019-src-magritte-a-modern-shell-language-short-presentation-"],
-                        description: "The ACM Student Research Competition (SRC), sponsored by Microsoft Research, offers a unique forum for ACM student members at the undergraduate and graduate levels to present their original research at &lt;Programming&gt; before a panel of judges and conference attendees, and creates opportunities for learning and networking among researchers and practitioners. The SRC gives visibility to not only up-and-coming young researchers, but also to the field of computer science research and its community.",
-                        type: "Invited Students",
-                        day: "2019-04-03",
-                        timeString: "09:30 - 11:00",
-                        startTime: DateTime.parse("2019-04-03 09:30"),
-                        location: "Hotel Bristol Palace | Bellini"
-                    ),
-                  ),
-
-                    SessionContainer(
-                      activity: Session(
-                          key: "dc03a3e9-5dbd-4af4-973c-0941b84ef00b",
-                          title: "Student Research Competition: Oral Presentation",
-                          chairString: "Philipp Haller",
-                          chairKey: "philipphaller",
-                          itemKeys: ["programming-2019-src-cantor-pairing-in-a-reversible-programming-language-short-presentation-",
-                            "programming-2019-src-reading-logic-as-code-or-as-natural-language-text-short-presentation-",
-                            "programming-2019-src-a-framework-for-big-step-semantics-short-presentation-",
-                            "programming-2019-src-rml-runtime-monitoring-language-short-presentation-",
-                            "programming-2019-src-baccaml-the-meta-hybrid-just-in-time-compiler-short-presentation-",
-                            "programming-2019-src-magritte-a-modern-shell-language-short-presentation-"],
-                          description: "The ACM Student Research Competition (SRC), sponsored by Microsoft Research, offers a unique forum for ACM student members at the undergraduate and graduate levels to present their original research at &lt;Programming&gt; before a panel of judges and conference attendees, and creates opportunities for learning and networking among researchers and practitioners. The SRC gives visibility to not only up-and-coming young researchers, but also to the field of computer science research and its community.",
-                          type: "Invited Students",
-                          day: "2019-04-03",
-                          timeString: "09:30 - 11:00",
-                          startTime: DateTime.parse("2019-04-03 09:30"),
-                          location: "Hotel Bristol Palace | Bellini"
-                      ),
-                    ),
-
-                    SessionContainer(
-                      activity: Session(
-                          key: "f3dc5937-50c4-48f8-a93b-b739fd7dae01",
-                          title: "Catering: Lunch",
-                          itemKeys: ["programming-2019-catering-lunch"],
-                          description: "Lunch, Dinner, Coffee-breaks",
-                          type: "Accepted Papers",
-                          day: "2019-04-04",
-                          timeString: "12:30 - 14:00",
-                          startTime: DateTime.parse("2019-04-04 12:30"),
-                          location: "Hotel Bristol Palace | ​Lunch area"
-                      ),
-                    ),
-
-                    SessionContainer(
-                      activity: Session(
-                          key: "f3dc5937-50c4-48f8-a93b-b739fd7dae01",
-                          title: "Catering: Lunch",
-                          itemKeys: ["programming-2019-catering-lunch"],
-                          description: "Lunch, Dinner, Coffee-breaks",
-                          type: "Accepted Papers",
-                          day: "2019-04-04",
-                          timeString: "12:30 - 14:00",
-                          startTime: DateTime.parse("2019-04-04 12:30"),
-                          location: "Hotel Bristol Palace | ​Lunch area"
-                      ),
-                    ),
-
-                    SessionContainer(
-                      activity: Session(
-                          key: "f3dc5937-50c4-48f8-a93b-b739fd7dae01",
-                          title: "Catering: Lunch",
-                          itemKeys: ["programming-2019-catering-lunch"],
-                          description: "Lunch, Dinner, Coffee-breaks",
-                          type: "Accepted Papers",
-                          day: "2019-04-04",
-                          timeString: "12:30 - 14:00",
-                          startTime: DateTime.parse("2019-04-04 12:30"),
-                          location: "Hotel Bristol Palace | ​Lunch area"
-                      ),
-                    ),
-
-                    SessionContainer(
-                      activity: Session(
-                          key: "f3dc5937-50c4-48f8-a93b-b739fd7dae01",
-                          title: "Research Papers: Live Programming",
-                          itemKeys: ["programming-2019-catering-lunch"],
-                          description: "Lunch, Dinner, Coffee-breaks",
-                          type: "Accepted Papers",
-                          day: "2019-04-04",
-                          timeString: "12:30 - 14:00",
-                          startTime: DateTime.parse("2019-04-04 12:30"),
-                          location: "Hotel Bristol Palace | ​Lunch area"
-                      ),
-                    ),
-
-                    SessionContainer(
-                      activity: Session(
-                          key: "f3dc5937-50c4-48f8-a93b-b739fd7dae01",
-                          title: "Catering: Lunch",
-                          itemKeys: ["programming-2019-catering-lunch"],
-                          description: "Lunch, Dinner, Coffee-breaks",
-                          type: "Accepted Papers",
-                          day: "2019-04-04",
-                          timeString: "12:30 - 14:00",
-                          startTime: DateTime.parse("2019-04-04 12:30"),
-                          location: "Hotel Bristol Palace | ​Lunch area"
-                      ),
-                    ),
-
-                    SessionContainer(
-                      activity: Session(
-                          key: "f3dc5937-50c4-48f8-a93b-b739fd7dae01",
-                          title: "Catering: Lunch",
-                          itemKeys: ["programming-2019-catering-lunch"],
-                          description: "Lunch, Dinner, Coffee-breaks",
-                          type: "Accepted Papers",
-                          day: "2019-04-04",
-                          timeString: "12:30 - 14:00",
-                          startTime: DateTime.parse("2019-04-04 12:30"),
-                          location: "Hotel Bristol Palace | ​Lunch area"
-                      ),
-                    ),
-         ]*/
-                    ),
+                    }),
               ),
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton.extended(
             onPressed: () {
-              // TODO: usar funcoes json para extrair as sessoes de cada dia
 
-              SplayTreeSet<Session> set = SplayTreeSet.from([
-                DayScheduleScreen.session3,
-                DayScheduleScreen.session1,
-                DayScheduleScreen.session2
-              ]);
+              // TODO: usar funcoes json para extrair as sessoes de cada dia
+              SplayTreeSet<Session> set = SplayTreeSet();
+
 
               Navigator.pushNamed(context, '/daySessionsScreen',
                   arguments:
