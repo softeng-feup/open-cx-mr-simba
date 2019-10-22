16959253a5227af1828500c8451f1a22671a8b03
luispcunha <luispplmcunha@gmail.com>
2019-10-22 17:44:08 +0100

    Change Session class

--- AMA/lib/data/Session.dart
+++ AMA/lib/data/Session.dart
@@ -1,15 +1,4 @@
 class Session {
-  Session({this.key,
-           this.title,
-           this.chairsString,
-           this.chairs,
-           this.items,
-           this.type,
-           this.day,
-           this.timeString,
-           this.location,
-           this.description});
-
   final String key;
   final String title;
   final String chairsString; // name of the chair
@@ -17,7 +6,37 @@ class Session {
   final List<String> items; // item keys, so we can get info about the these items in the json file
   final String type;
   final String day;
+  final DateTime startTime;
   final String timeString;
   final String location;
   final String description;
+
+  Session({this.key,
+           this.title,
+           this.chairsString,
+           this.chairs,
+           this.items,
+           this.type,
+           this.day,
+           this.startTime,
+           this.timeString,
+           this.location,
+           this.description});
+
+  @override
+  bool operator==(other) {
+    return this.key == other.key;
+  }
+
+  @override
+  int get hashCode => this.key.hashCode;
+
+  @override
+  int compareTo(other) {
+    if (this.key == other.key) return 0;
+    
+    return this.timeString.compareTo(other.timeString);
+  }
 }
+
+
