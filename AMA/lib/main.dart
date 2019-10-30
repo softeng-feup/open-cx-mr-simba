060c7fe8b0baab33aec8c307e8d77220acc8f3dc
EduRibeiro00 <eribeiro306@gmail.com>
2019-10-30 22:09:20 +0000

    Refresh button; and close slidable

--- AMA/lib/main.dart
+++ AMA/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:ama/controller/Controller.dart';
 import 'package:flutter/material.dart';
 
 void main() async { 
-  await Controller.instance().getJson();
+  Controller.instance().getJson();
   runApp(MyApp());
 }
 
