78cf605468af0342c23163eec735e0c2ee8fc01b
David Silva <up201705373@fe.up.pt>
2019-11-05 12:26:00 +0000

    Working on bluetooth

--- AMA/lib/controller/bluetooth/BluetoothController.dart
+++ AMA/lib/controller/bluetooth/BluetoothController.dart
@@ -2,16 +2,13 @@ import 'package:flutter_blue/flutter_blue.dart';
 import '../../constants/Utility.dart' as Utility;
 
 class BluetoothController {
-
   static BluetoothController _instance;
 
   static BluetoothController instance() {
-    if(_instance == null)
-        _instance = new BluetoothController();
+    if (_instance == null) _instance = new BluetoothController();
 
     return _instance;
   }
-
 
   // TODO: FlutterBlue.instance nao retorna quando nao ha BT... ver melhor
 
@@ -23,12 +20,27 @@ class BluetoothController {
     return await FlutterBlue.instance.isOn;
   }
 
-
   void searchForBeacons() {
+    // Start scanning
+    FlutterBlue.instance.startScan(timeout: Duration(seconds: 4));
+
+    // Listen to scan results
+    var scannedDevices;
+    var subscription = FlutterBlue.instance.scanResults.listen((scannedDevices) {
+
+      for(int i = 0; i < scannedDevices.length;i++){
+        // do something with scan result
+        var currentDevice = scannedDevices.elementAt(i).device; // found usefulness
+        var currentAdvertisementData = scannedDevices.elementAt(i).advertisementData; 
+
+        // print('${device.id} found! rssi: ${scannedDevices.elementAt(i).rssi} uuid: ${scannedDevices.elementAt(i).advertisementData.serviceUuids.elementAt(0)}');
+      }
+      
+    });
+
+// Stop scanning
+    FlutterBlue.instance.stopScan();
 
     // this._flutterBlue.startScan(timeout: Duration(seconds: Utility.numSecondsForTimeoutBLE));
-
-
   }
-
 }
