03cf74babaa3fc0b5a506a21c47c88c52fb7a0e2
José Gomes <zemiguelgomes99@gmail.com>
2019-11-07 16:26:17 +0000

    Started DB access

--- AMA/lib/controller/database/DatabaseMapper.dart
+++ AMA/lib/controller/database/DatabaseMapper.dart
@@ -1,9 +1,24 @@
 import 'dart:collection';
+import 'package:path/path.dart';
 import 'package:ama/model/Session.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseController {
   // TODO: fazer metodos que extraiam informacao da base de dados e converta para classes, para mostrar na View
+  static DatabaseController _instance;
+
+  static DatabaseController instance() {
+    if (_instance == null) _instance = new DatabaseController();
+      return _instance;
+  }
+
+  createDatabase() async {
+    String databasesPath = await getDatabasesPath();
+    String dbPath = join(databasesPath, 'AMADatabase.db');
+
+    var database = await openDatabase(dbPath, version: 1);
+    return database;
+  }
 
   // exemplo:
   static Future<SplayTreeSet<Session>> sessionSet(Database db, String date) async {
