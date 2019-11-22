05e6708737ad87892b79c92cd278ce10a2523ad5
luispcunha <luispplmcunha@gmail.com>
2019-11-22 23:04:12 +0000

    Search sessions by title

--- AMA/lib/view/screens/DaySessionsScreen.dart
+++ AMA/lib/view/screens/DaySessionsScreen.dart
@@ -24,31 +24,68 @@ class DaySessionsScreen extends StatelessWidget {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: this.drawSessionsList(),
+      body: SearchSessions(sessionsInfo),
     );
   }
+}
 
+class SearchSessions extends StatefulWidget {
+  final DayScheduleInfo sessionsInfo;
 
-  Widget drawSessionsList() {
-    return Container(
-        color: AppColors.backgroundColor,
-        child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            padding: const EdgeInsets.only(
-                left: 10.0, right: 10.0, top: 10.0, bottom: 10.0),
-            itemCount: sessionsInfo.getSessions().length,
-            itemBuilder: (context, idx) {
-              return SlidableSessionContainer(
-                session: sessionsInfo.getSessions().elementAt(idx),
-                icon: Icons.check,
-                color: Colors.green,
-                onPressFunction: () async {
-                  String text = await Controller.instance().addSessionToSchedule(sessionsInfo.getDay(), sessionsInfo.getSessions().elementAt(idx));
-                    Scaffold.of(context)
-                        .showSnackBar(SnackBar(content: Text(text)));
-                },
-              );
-            })
-    );
+  const SearchSessions(this.sessionsInfo);
+
+  @override
+  State<StatefulWidget> createState() => new SearchSessionsState(sessionsInfo);
+}
+
+class SearchSessionsState extends State<SearchSessions> {
+  final DayScheduleInfo sessionsInfo;
+  TextEditingController controller = new TextEditingController();
+  String filter;
+
+  SearchSessionsState(this.sessionsInfo) {
+    controller.addListener(() {
+      setState(() {
+        filter = controller.text;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Padding(
+        padding: new EdgeInsets.only(left: 20.0, right: 20.0),
+        child: TextField(
+            decoration: new InputDecoration(labelText: "Search"),
+            controller: controller),
+      ),
+      Expanded(
+          child: Container(
+              color: AppColors.backgroundColor,
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  padding: const EdgeInsets.only(
+                      left: 10.0, right: 10.0, top: 10.0, bottom: 10.0),
+                  itemCount: sessionsInfo.getSessions().length,
+                  itemBuilder: (context, idx) {
+                    if (filter == null ||
+                        filter == "" ||
+                        sessionsInfo.getSessions().elementAt(idx).title.toLowerCase().contains(filter.toLowerCase()))
+                      return SlidableSessionContainer(
+                        session: sessionsInfo.getSessions().elementAt(idx),
+                        icon: Icons.check,
+                        color: Colors.green,
+                        onPressFunction: () async {
+                          String text = await Controller.instance()
+                              .addSessionToSchedule(sessionsInfo.getDay(),
+                                  sessionsInfo.getSessions().elementAt(idx));
+                          Scaffold.of(context)
+                              .showSnackBar(SnackBar(content: Text(text)));
+                        });
+                    else
+                      return Container();
+
+                  })))]);
   }
 }
